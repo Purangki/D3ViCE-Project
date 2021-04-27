5a86c514b292751ba0c19aef856f4bb7b4b28118
Abigail Portuito <abigailportuito@gmail.com>
2021-04-27 10:43:02 +0800

    D3ViCE_Conference Documentation
    -models.py
    -version1

--- D3ViCE_Conference/models.py
+++ D3ViCE_Conference/models.py
@@ -6,14 +6,14 @@ from D3ViCE_User.models import Sponsor
 # # Create your models here.
 
 class Conference(models.Model):
-	code = models.CharField(max_length = 50)
-	title = models.CharField(max_length = 150)
-	date = models.DateField()
-	time = models.TimeField()
-	description = models.CharField(max_length = 255)
-	is_created = models.BooleanField(default = False) #True - user is host of (or created the) conference, False - user is just a participant
-	is_deleted = models.BooleanField(default = False)
-	status = models.CharField(max_length = 50, default = "Not Started") #added by Abby for the main dashboard
+	code = models.CharField(max_length = 50)	#conference code
+	title = models.CharField(max_length = 150)	#conference title
+	date = models.DateField()	#conference date
+	time = models.TimeField()	#conference time
+	description = models.CharField(max_length = 255)	#conference description 
+	is_created = models.BooleanField(default = False) 	#True - user is host of (or created the) conference, False - user is just a participant
+	is_deleted = models.BooleanField(default = False)	#archives the conference, deleted in the page but not in the db
+	status = models.CharField(max_length = 50, default = "Not Started")	#added by Abby for the main dashboard
 	participant = models.ManyToManyField(Participant)
 	sponsor = models.ForeignKey(Sponsor, on_delete=models.CASCADE, related_name = 'sponsor', null = True, blank = True)
 	# host = models.ForeignKey(Host, on_delete = models.SET_NULL, null = True, related_name = "all_conferences") #added by Abby 
