08282a15882177b6a57011005beb45d35705b520
Abigail Portuito <abigailportuito@gmail.com>
2021-04-27 10:36:27 +0800

    D3ViCE_Account Documentation
    -views.py and urls.py
    -version1

--- D3ViCE_Account/urls.py
+++ D3ViCE_Account/urls.py
@@ -3,6 +3,6 @@ from D3ViCE_Account import views
 
 app_name = 'D3ViCE_Account'
 urlpatterns = [
-	path('login', views.UserLoginView.as_view(), name="login"),
-	path('logout', views.LogoutView.as_view(), name="logout"),
+	path('login', views.UserLoginView.as_view(), name="login"), #url towards the login view of the User
+	path('logout', views.LogoutView.as_view(), name="logout"),	#url towards the logout view of the User
 ] 
--- D3ViCE_Account/views.py
+++ D3ViCE_Account/views.py
@@ -12,24 +12,24 @@ from django.contrib.auth.models import auth, User #builtin django user
 from .models import *
 
 # Create your views here.
-class UserLoginView(View):
-    def get(self, request):
-        return render(request, '5_Login.html')
+class UserLoginView(View):  #view of user when he logs in to the system                    
+    def get(self, request): #get method for the template
+        return render(request, '5_Login.html')  #render() returns a loaded template
 
-    def post(self, request):
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = auth.authenticate(username = username, password = password)
-        if user is not None:
-            auth.login(request, user)
-            currentUser = user
-            print(str(currentUser.id) + " is logged in")
-            context = {
+    def post(self, request):    #post method   
+        username = request.POST.get("username") #username 
+        password = request.POST.get("password") #user password
+        user = auth.authenticate(username = username, password = password)  #using django's builtin authentication system, username & password is checked
+        if user is not None:    #if user is not None then
+            auth.login(request, user)   #user is logged in on the system
+            currentUser = user  #currentUser means that whoever is logged is in that system is the user at present, if this is not set then it is an anonymous user 
+            print(str(currentUser.id) + " is logged in")    #prints the current user logged in in the terminal
+            context = { #context will be used for queries
             'current_user' : currentUser
             }
-            return redirect("D3ViCE_Conference:dashboard_view")
+            return redirect("D3ViCE_Conference:dashboard_view") #redirects the user to the User dashboard
         else:
-            return HttpResponse("wrong credentials")
+            return HttpResponse("wrong credentials")    #otherwise if user does not exist and have the wrong credentals then it displays this error
 
 class LogoutView(View):
     def get(self,request):
@@ -37,7 +37,7 @@ class LogoutView(View):
         return  redirect("D3ViCE_Account:login")
     def post(self,request):
         logout(request)
-        return  redirect("D3ViCE_Account:login")
+        return  redirect("D3ViCE_Account:login")    #if no user is logged in, he cannot go the dashboard even if he manually types the url in the browser
 
 
 
