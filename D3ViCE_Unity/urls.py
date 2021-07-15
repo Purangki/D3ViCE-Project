7ea913d1f55e7933fab1aa2006ab176d5c82bcaa
maryvillondo <maryvillondo@gmail.com>
2021-07-15 16:00:25 +0800

    Solved: Unity-Django Login

--- D3ViCE_Unity/urls.py
+++ D3ViCE_Unity/urls.py
@@ -5,8 +5,8 @@ from . import views
 #path arranged alphabetically by name
 app_name = 'D3ViCE_Unity'
 urlpatterns=[
-    path('unity/login/<slug:username>/', login_user, name="unitylogin"),
-    #path('unity/login/', login_user, name="unitylogin"),
+    #path('unity/login/<slug:username>/', login_user, name="unitylogin"),
+    path('unity/login/', login_user, name="unitylogin"),
     path('unity/update_avatar/', update_avatar, name="unityupdate"),
     path('unity/join/<slug:code>/', join_conference, name="unityjoin"),
     path('unity/participant/', register_participant, name="unityparticipant"),
