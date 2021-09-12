f9f64e1aee0a807cc1bc7b35790ee24b976bece5
Abigail Portuito <abigailportuito@gmail.com>
2021-09-12 10:15:21 +0800

    dashboard additional info
    -show more modal

--- D3ViCE_Conference/models.py
+++ D3ViCE_Conference/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from datetime import datetime
-# from django.contrib.auth.models import auth, User
-# from D3ViCE_User.models import Participant
-# from D3ViCE_User.models import Sponsor
-# from D3ViCE_User.models import Host
-# from D3ViCE_User.models import Speaker
 from D3ViCE_User.models import Profile
-# # Create your models here.
-
 class Conference(models.Model):
 	code = models.CharField(max_length = 50)	#conference code
 	title = models.CharField(max_length = 150)	#conference title
@@ -21,8 +14,6 @@ class Conference(models.Model):
 	seats = models.IntegerField()
 	type = models.CharField(max_length = 255)
 
-	# sponsor = models.ManyToManyField(Profile, related_name = 'sponsored_conferences', blank = True)
-	# question = models.ForeignKey(Profile, on_delete=models.SET_NULL, related_name="question_conference", null=True, blank= True)
 	host = models.ForeignKey(Profile, on_delete=models.SET_NULL, related_name="created_conferences", null=True, blank = True)
 	participant = models.ManyToManyField(Profile, related_name = "joined_conferences", blank = True)
 	speaker = models.ManyToManyField(Profile, related_name = "speaker_conferences", blank = True)
